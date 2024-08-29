7e126af3a3587e60f1eb86d919b0470f973daf6e
beatrizdaddea <beatrizchith@gmail.com>
2024-08-29 20:18:17 -0300

    feat: config endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import cors from "cors";
 
 import { appConfig } from "./server/config/appConfig";
 import { aiController } from "./server/controller/aiController";
+import { listReadingsController } from "./server/controller/listReadingsController";
+/* import { confirmReadingController } from "./server/controller/patchReadingController"; */
 import { validateRequestBody } from "./server/middleware/validation";
+
+import sequelize from './server/config/database';
 
 const app = express();
 
@@ -14,8 +18,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
+sequelize.sync().then(() => {
+  console.log('Database synchronized');
+}).catch((error) => {
+  console.error('Error synchronizing the database:', error);
+});
+
 // Define the endpoint with validation middleware
 app.post("/water-gas-reading", validateRequestBody, aiController);
+app.get("/:customer_code/list", listReadingsController);
+/* app.patch("/water-gas-reading/confirm", confirmReadingController); */
 
 // App listening
 app.listen(PORT, () => {
